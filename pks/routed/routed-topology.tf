f08ea84462f03164bbb329eefe24dc45aeba7736
nvpnathan <nathan79@gmail.com>
2019-02-05 09:57:50 -0700

    Added "outputs" for the UUIDs for the PKS tile

--- pks/routed/routed-topology.tf
+++ pks/routed/routed-topology.tf
@@ -307,3 +307,20 @@ resource "nsxt_ip_pool" "VIP_IP_POOL1" {
   }
 }
 
+## Output UUIDs of T0, IP Blocks, IP Pools
+
+output "T0_ROUTER_ID" {
+  value = "${nsxt_logical_tier0_router.TIER0_ROUTER.id}"
+}
+
+output "NODES_IP_BLOCK" {
+  value = "${nsxt_ip_block.node_ip_block.id}"
+}
+
+output "PODS_IP_BLOCK" {
+  value = "${nsxt_ip_block.pod_ip_block.id}"
+}
+
+output "VIP_IP_POOL" {
+  value = "${nsxt_ip_pool.VIP_IP_POOL1.id}"
+}
